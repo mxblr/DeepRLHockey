c28afe79bb1583880174c8f9e4312946b75b6377
mxblr <maximilian.beller@web.de>
2025-04-05 23:11:45 +0200

    Add loss calculation

--- src/v2/sac/sac.py
+++ src/v2/sac/sac.py
@@ -53,10 +53,10 @@ class SoftActorCriticConfig:
     def __init__(
         self,
         tau: float = 0.005,
-        lambda_V: float = 3e-4,
-        lambda_Q: float = 3e-4,
-        lambda_Pi: float = 3e-4,
-        lambda_Alpha: float = 3e-4,
+        learning_rate_v: float = 3e-4,
+        learning_rate_q: float = 3e-4,
+        learning_rate_pi: float = 3e-4,
+        learning_rate_alpha: float = 3e-4,
         discount: float = 0.99,
         target_update=1,
         buffer_size: int = int(1e6),
@@ -66,15 +66,15 @@ class SoftActorCriticConfig:
         dim_obs: int = 16,
         alpha: typing.Union[str, float] = "auto",
         target_entropy: typing.Union[str, float] = "auto",
-        q_fct_config: dict = None,
-        v_fct_config: dict = None,
-        pi_fct_config: dict = None,
+        q_fct_config=None,
+        v_fct_config=None,
+        pi_fct_config=None,
     ):
         self.tau = tau
-        self.lambda_V = lambda_V
-        self.lambda_Q = lambda_Q
-        self.lambda_Pi = lambda_Pi
-        self.lambda_Alpha = lambda_Alpha
+        self.learning_rate_v = learning_rate_v
+        self.learning_rate_q = learning_rate_q
+        self.learning_rate_pi = learning_rate_pi
+        self.lambda_Alpha = learning_rate_alpha
         self.discount = discount
         self.target_update = target_update
         self.buffer_size = buffer_size
@@ -101,8 +101,8 @@ class SoftActorCritic(nn.Module):
         config: SoftActorCriticConfig,
         input_space,
         action_space,
-        value_fct: nn.Module,
-        policy_fct: nn.Module,
+        ValueFunction: nn.Module,
+        PolicFunction: nn.Module,
         env,
     ):
         """
@@ -134,8 +134,12 @@ class SoftActorCritic(nn.Module):
             size=self.config.buffer_size,
         )
 
-        self.value_function = value_fct
-        self.policy_function = policy_fct
+        # set up the value, policy and q-function networks
+        self.Q1 = ValueFunction(self.config.v_fct_config)
+        self.Q2 = ValueFunction(self.config.v_fct_config)
+        self.Policy = PolicFunction(self.config.pi_fct_config)
+        self.V = ValueFunction(self.config.v_fct_config)
+        self.V_target = ValueFunction(self.config.v_fct_config)
 
         # Alpha can also be trained
         self.target_entropy = self.config.target_entropy
@@ -158,10 +162,12 @@ class SoftActorCritic(nn.Module):
         self._sess.run(self._update_target_V_ops_hard)  # TODO
 
     def load_weights(self, filepath):
-        pass
+        # TODO
+        raise NotImplementedError("Loading model weights is not implemented yet")
 
     def save_weights(self, filepath):
-        pass
+        # TODO
+        raise NotImplementedError("Saving model is not implemented yet.")
 
     def action(self, observation):
         """
@@ -192,12 +198,57 @@ class SoftActorCritic(nn.Module):
         return action
 
     def forward(self, observation, action, reward, observation_new, env_done):
-        loss_v_fct, loss_q1_fct, loss_q2_fct, loss_pi_fct = None, None, None, None
-        return loss_v_fct, loss_q1_fct, loss_q2_fct, loss_pi_fct
+        """Calculate losses for a given set of observations, actions and rewards."""
+        q1 = self.Q1(torch.cat([observation, action], dim=-1))
+        q2 = self.Q2(torch.cat([observation, action], dim=-1))
+        pi_action, pi_log_prob, pi_log_std, pi_mu = self.Policy(observation)
+
+        # calculate value of current and new observation
+        v = self.V(observation)
+        v_target = self.V(observation_new)
+
+        # Q function values for new actions
+        q1_pi = self.Q1(torch.cat([observation, pi_action], dim=-1))
+        q2_pi = self.Q2(torch.cat([observation, pi_action], dim=-1))
+
+        # probability of actions sampled from Multivariate normal
+        log_prob_new_act = pi_log_prob
+        # Uniform normal assumed - therefore prior is 0
+        log_prob_prior = 0.0
+
+        # Target for Q function
+        target_q = reward + self.config.discount * (1 - env_done) * v_target.detach()
+        q1_loss = 0.5 * torch.mean(torch.pow(target_q - q1, 2))
+        q2_loss = 0.5 * torch.mean(torch.pow(target_q - q2, 2))
+
+        # Target for value network
+        min_q1_q2 = torch.minimum((q1_pi, q2_pi))
+        target_v = min_q1_q2.detach() - self.alpha.detach() * (log_prob_new_act.detach() + log_prob_prior)
+        v_loss = 0.5 * torch.mean(torch.pow(v - target_v, 2))
+
+        # PI update
+        pi_loss_kl = torch.mean(self.alpha * log_prob_new_act - q1_pi)
+        policy_regularization_loss = (
+            0.001 * 0.5 * (torch.mean(torch.pow(pi_log_std, 2)) + torch.mean(torch.pow(pi_mu, 2)))
+        )
+        pi_loss = pi_loss_kl + policy_regularization_loss
+
+        if self.train_alpha:
+            _alpha_loss = -torch.mean(self.log_alpha * log_prob_new_act.detach() + self.target_entropy.detach())
+            raise NotImplementedError("Please implement the dynamic calcuation of the alpha loss.")
+
+        return v_loss, q1_loss, q2_loss, pi_loss
 
     def train_from_buffer(self):
         observation, action, reward, observation_new, env_done = self.buffer.sample_batch(self.config.batch_size)
         return self.forward(observation, action, reward, observation_new, env_done)
+
+    @staticmethod
+    def do_optimizer_step(optimizer, loss):
+        optimizer.zero_grad()
+        loss.backward()
+        # update weights
+        optimizer.step()
 
     def train(
         self,
@@ -221,6 +272,12 @@ class SoftActorCritic(nn.Module):
         bar = progressbar.ProgressBar(max_value=epochs)
         # Initialize training statistics
         history = TrainingHistory()
+        optimizers = {
+            "q1": torch.optim.Adam(self.Q1.parameters(), lr=self.config.learning_rate_q),
+            "q2": torch.optim.Adam(self.Q2.parameters(), lr=self.config.learning_rate_q),
+            "v": torch.optim.Adam(self.V.parameters(), lr=self.config.learning_rate_v),
+            "pi": torch.optim.Adam(self.Policy.parameters(), lr=self.config.learning_rate_pi),
+        }
 
         # start training
         total_steps = 0
@@ -254,8 +311,18 @@ class SoftActorCritic(nn.Module):
                 if total_steps >= self.config.batch_size:
                     for _gradient_step in range(grad_steps):
                         # train the actor and critic models
-                        loss_v_fct, loss_q1_fct, loss_q2_fct, loss_pi_fct = self.train_from_buffer()
-                        history.update(loss_pi=loss_pi_fct, loss_q1=loss_q1_fct, loss_q2=loss_q2_fct, loss_v=loss_v_fct)
+                        loss_v, loss_q1, loss_q2, loss_pi = self.train_from_buffer()
+                        self.do_optimizer_step(optimizers["q1"], loss_q1)
+                        self.do_optimizer_step(optimizers["q2"], loss_q2)
+                        self.do_optimizer_step(optimizers["v"], loss_v)
+                        self.do_optimizer_step(optimizers["pi"], loss_pi)
+
+                        history.update(
+                            loss_pi=loss_pi.numpy(),
+                            loss_q1=loss_q1.numpy(),
+                            loss_q2=loss_q2.numpy(),
+                            loss_v=loss_v.numpy(),
+                        )
                 total_steps += 1
                 if env_done:
                     break
