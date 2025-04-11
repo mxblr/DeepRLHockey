07595695ab70e0307e18282019309c8ec89f22c0
mxblr <maximilian.beller@web.de>
2025-04-11 20:56:17 +0200

    Add unittest to make sure policy learns

--- tests/test_v2/test_policy.py
+++ tests/test_v2/test_policy.py
@@ -21,3 +21,24 @@ class TestNormalPolicy(TestCase):
 
         for out in other_outputs:
             self.assertIsInstance(out, torch.Tensor)
+
+    def test_overfitting(self):
+        config = NormalPolicyFunctionConfig()
+        policy_network = NormalPolicyFunction(config)
+        inpt = torch.rand((256, config.input_dim))
+        q1_pi = torch.rand((256, config.input_dim))
+        optimizer = torch.optim.Adam(policy_network.parameters(), lr=3e-4)
+        _alpha_no_grad = 0.1
+
+        for step in range(100):
+            pi_action, log_prob_new_act, _, pi_log_std, pi_mu = policy_network(inpt)
+            pi_loss_kl = torch.mean(_alpha_no_grad * log_prob_new_act - q1_pi)
+            policy_regularization_loss = (
+                0.001 * 0.5 * (torch.mean(torch.pow(pi_log_std, 2)) + torch.mean(torch.pow(pi_mu, 2)))
+            )
+            pi_loss = pi_loss_kl + policy_regularization_loss
+            print(f"{step}: {pi_loss=} | {pi_loss_kl=} | {policy_regularization_loss=}")
+            optimizer.zero_grad()
+            pi_loss.backward()
+            optimizer.step()
+        self.assertLess(pi_loss.detach().numpy(), 0.001)
